4b59e1356f9af6bc06567ca88a7119af9b72bfdf
zuc04 <paolo.zucconi04@gmail.com>
2026-03-25 22:56:30 +0100

    Add files via upload

---
+++ Inc/pmst_nmea_CAN_headers.h
@@ -0,0 +1,18 @@
+#ifndef pmst_nmea_CAN_headers_H
+#define pmst_nmea_CAN_headers_H
+
+//PMST definitions
+#define PRIORITY 0
+#define MANUFACTURERCODE 1365
+#define INDUSTRYMARINE 4
+
+//PGN definitions
+#define GPS_MINIMAL_PGN 0x1F805
+#define IMU_ATTITUDE_PGN 127257
+#define IMU_ACCELERATION_PGN 0xFF02
+#define LIKA_SAILANGLE_PGN 0xFF00
+#define POTENTIOMETER_DATA_PGN 0xFF01
+#define WINDEX_WINDDATA_PGN 0x1FD02
+#define FOIL_ANGLE_PGN 0x1FD10
+
+#endif
